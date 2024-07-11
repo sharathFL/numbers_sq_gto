888663cad17e55939c8a02b9ead31aef610fa2cf
sharathFL <sharath@finiteloop.io>
2024-07-11 09:21:21 +0000

    fourth model

---
+++ src/train_model_v4.py
@@ -0,0 +1,17 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+# Load data
+data = pd.read_csv('/code/data/data.csv')
+X = data[['X']]
+y = data['y']
+
+# Train model with a slight modification
+model = LinearRegression()
+model.fit(X, y)
+print("Intercept:", model.intercept_)
+print("Coefficient:", model.coef_)
+
+# Save model
+joblib.dump(model, 'model_v4.joblib')
