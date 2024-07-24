a82c230c69f7d6c78bde8fdb920f00d4113d03d8
sharathFL <sharath@finiteloop.io>
2024-07-24 16:24:03 +0000

    dvc artifacts+dvc push

---
+++ src/train_model_v5.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+import joblib
+
+# Load data
+data = pd.read_csv('/code/data/data.csv')
+X = data[['X']]
+y = data['y']
+
+# Train model with a slight modification
+model = LinearRegression()
+# model.fit(X, y)
+model.fit(X*X*X*X,y)
+print("Intercept:", model.intercept_)
+print("Coefficient:", model.coef_)
+
+# Save model
+joblib.dump(model, '/code/models/model_v5.joblib')
