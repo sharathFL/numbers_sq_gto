c11d6f5583ffb9c08bff01cc0b33a20be8497b0a
sharathFL <sharath@finiteloop.io>
2024-07-25 09:37:22 +0000

    10th!

--- src/train_model_v5.py
+++ src/train_model_v5.py
@@ -3,16 +3,16 @@ from sklearn.linear_model import LinearRegression
 import joblib
 
 # Load data
-data = pd.read_csv('/code/data/data.csv')
+data = pd.read_csv('data/data.csv')
 X = data[['X']]
 y = data['y']
 
 # Train model with a slight modification
 model = LinearRegression()
 # model.fit(X, y)
-model.fit(X*X*X*X*X*X*X*X*X*X*X,y)
+model.fit(X*X*X*X*X*X*X*X*X*X*X*X,y)
 print("Intercept:", model.intercept_)
 print("Coefficient:", model.coef_)
 
 # Save model
-joblib.dump(model, '/code/models/wip_1_model.joblib')
+joblib.dump(model, 'models/wip_1_model.joblib')
